d27ed24080ef4281bb0c3a99a9618a3efa18dc41
Raquel <calderon.rocky@gmail.com>
2016-02-11 21:34:11 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,16 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	var url = "/project/"+idNumber;
+	console.log(url);
+	$.get(url, function(result){
+		var details = $('#'+projectID).find('.thumbnail').find('div.details');
+		console.log(result);
+		var projectHTML = '<img src="'+result['image'] +
+		'"" class="detailsImage img">' + '<h6>' + result['date'] + '</h6>'+
+		'<p>' + result['summary'] +'</p>'
+		details.html(projectHTML);
+	})
 }
 
 /*
@@ -34,5 +44,12 @@ function addProjectDetails(e) {
  * and apply it
  */
 function randomizeColors(e) {
-	console.log("User clicked on color button");
+	$.get('/palette', function(randomPalette){
+			var colors = randomPalette["colors"]["hex"];
+		$('body').css('background-color', colors[0]);
+		$('.thumbnail').css('background-color', colors[1]);
+		$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+		$('p').css('color', colors[3]);
+		$('.project img').css('opacity', .75);
+	})
 }
